08a9c113e0f6534b086972fe6bd0b2cd5ef55056
quoc <squoc.nguyen@gmail.com>
2021-08-07 22:44:37 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('hello world'));
+
+app.listen(port, () => console.log('server running on port ' + port));
